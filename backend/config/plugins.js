c103e92eb90f7c2f6d236b7e86aa957add0df42a
matthijswillems <willemsmatthijs@gmail.com>
2024-01-13 15:51:32 +0100

    sync

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -2,4 +2,14 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
+  "strapi-neon-tech-db-branches": {
+    enabled: true,
+    config: {
+      neonApiKey:
+        "b75wrppmr2gq6u8o70ixgz23cw47itn6iwx9gnnnsiwwp31lvxcm13ieip806uvx",
+      neonProjectName: "strapi-portfolio",
+      neonRole: "matthijswillems",
+      gitBranch: env.GIT_BRANCH,
+    },
+  },
 });
